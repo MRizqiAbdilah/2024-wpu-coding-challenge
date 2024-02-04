68ca0fa84f5cbc581ecad21a8252b926efc4d7de
MRizqiAbdilah <muhammadrizqiabdilah10@gmail.com>
2024-02-05 04:14:53 +0700

    day 4/366

---
+++ 4-smash.py
@@ -0,0 +1,15 @@
+# WPU Coding Challenge 2024
+# 4/366
+# https://www.codewars.com/kata/59ca8246d751df55cc00014c
+
+# way 1
+def smash(words):
+    join_words = " ".join(words)
+    return join_words
+
+# way 2
+def smash(words):
+    return " ".join(words)
+
+# way 3
+smash = lambda words: " ".join(words)
